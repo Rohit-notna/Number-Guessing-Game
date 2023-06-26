25ea83e2d1356e84cb079656b8b7d0b07af2f240
Rohit-notna <134230552+Rohit-notna@users.noreply.github.com>
2023-06-26 13:37:02 +0530

    Click me

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import { useRef, useState } from 'react';
 
 function App() {
+  const [value, setvalue] = useState(Math.floor(Math.random() * 10) + 1);
+  const   para = useRef();
+  const inp = useRef();
+  function guessn(){ 
+     if(parseInt(inp.current.value) === value){
+      para.current.innerHTML="Congratulations! you win";
+      para.current.style.color="green";
+    }
+    else if(parseInt(inp.current.value)<value){
+      para.current.innerHTML="Too low!";
+      para.current.style.color="red";
+    }
+    else if(parseInt(inp.current.value)>value){
+      para.current.innerHTML="Too high!";
+      para.current.style.color="red";
+    }
+  }
+   function reset(){
+    para.current.innerHTML="";
+    setvalue(Math.floor(Math.random() * 10) + 1);
+    inp.current.value="";
+   }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <div className="container-fluid">
+      <div className="col-sm-6 mx-auto">
+      <div className="col-sm-12 mx-auto">
+      <h1 className='text-center'>Guess the Number</h1>
+      </div>
+      <div className="col-sm-4 mx-auto mt-4">
+        <p className="mt-4 mx-auto" ref={para}>Enter a number:</p>
+        <input type="number" class="form-control" className='mt-3' ref={inp}></input>
+      </div>
+      <div className="col-sm-4 mx-auto mt-4">
+      <button type="button" class="btn btn-success btn-lg" onClick={guessn}>Guess</button>
+        </div>
+        <div className="col-sm-4 mx-auto mt-2">
+        <button type="button" class="btn btn-danger btn-lg" onClick={reset}>Reset</button>
+        </div>
+      </div>
+     
+     </div>
   );
 }
 
